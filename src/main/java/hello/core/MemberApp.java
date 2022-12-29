f62e922e6075720cf49767730d8ef5b03c968a73
upcake <bermwoo@gmail.com>
2022-12-29 10:58:20 +0900

    관심사의 분리 : 생성자 주입으로 변경 및 테스트클래스 변경

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,14 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
 
 public class MemberApp {
 
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
-//        MemberService memberService = new MemberServiceImpl();
         MemberService memberService = appConfig.memberService();
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
